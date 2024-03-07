da714ac15ccde1001b4a87db19a8bc80ece38ce6
michael <michaelvx@gmail.com>
2024-03-07 02:39:09 +0200

    initial commit

---
+++ reflectiz-ui/src/app/shared/services/api/alerts-api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { AlertRes } from "../../../features/alerts/domain/alerts";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertsApiService {
+  constructor(private http: HttpClient)  {}
+  pullAlerts(): Observable<AlertRes> {
+    return this.http.get<AlertRes>('http://localhost:3000/alerts');
+  }
+}
